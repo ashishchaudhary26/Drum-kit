5c27d1f6cee2367e8c8fce527be1a049c4512da3
ashishchaudhary26 <142055283+ashishchaudhary26@users.noreply.github.com>
2023-09-15 21:40:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+// var numberOfbuttons =documemt.querySelectorAll(".drum").length;
+// for(var i=0;i<numberOfbuttons;i++){
+// document.querySelectorAll(".drum")[i].addEventListener("click" , function() {
+//     alert("i got clicked");
+// });
+// }
+var numberOfButtons=document.querySelectorAll(".drum").length;
+for(var i=0;i<numberOfButtons;i++){
+document.querySelectorAll(".drum")[i].addEventListener("click" ,function() {
+   
+        // alert("i got clicked");
+        var buttonInnerHTML= this.innerHTML;
+        makeSound(buttonInnerHTML);
+       
+        buttonAnimation(buttonInnerHTML);
+});
+}
+
+    document.addEventListener("keypress",function(Event){
+        makeSound(Event.key)
+
+        buttonAnimation(Event.key);
+    });
+
+function makeSound(key){
+        switch(key){
+         
+                case "w":
+                var crash=new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+       
+                case "a":
+                var kick=new Audio("sounds/kick-bass.mp3");
+                kick.play();
+                break;
+       
+                case "s":
+                var snare=new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+       
+                case "d":
+                var tom1=new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+       
+                case "j":
+                var tom2=new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+       
+                case "k":
+                var tom3=new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+       
+                case "l":
+                var tom4=new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+                
+                default:
+                       console.log(buttonInnerHTML);
+       
+               
+
+               }
+        }
+       function buttonAnimation(currentKey){
+        var activebutton= document.querySelector("."+currentKey);
+
+        activebutton.classList.add("pressed");
+
+        setTimeout(function(){
+                activebutton.classList.remove("pressed")
+        },100)
+       }
+
